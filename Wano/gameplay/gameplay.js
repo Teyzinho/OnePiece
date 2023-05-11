980f3776e046eef75c0e8bc56447065d5951e05b
Thiago <teyextreme@outlook.com>
2023-05-11 10:07:36 -0300

    Frame

--- Wano/gameplay/gameplay.js
+++ Wano/gameplay/gameplay.js
@@ -1,10 +1,18 @@
 let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext('2d'); //Contexto da imagem
 
+// Definindo a cor de fundo
+var corFundo = '#f1f1f1';
+
+// Preencher o fundo com a cor especificada
+ctx.fillStyle = corFundo;
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+
 let luffybase = new Image(); //Declara nova imagem
 luffybase.src = "sprites/luffy-base-sprites-transparente.png"; //Declara o local da imagem
 let kaidoBase = new Image();
-kaidoBase.src = "sprites/kaido_human.png";
+kaidoBase.src = "sprites/kaido_human_transparente.png";
 
 //Luffy
 let posSpriteX = 0; //Valor da posiçãoX da imagem
@@ -21,34 +29,57 @@ let movimento = "parado";
 //Kaido
 let KposSpriteX = 30; //Valor da posiçãoX da imagem
 let KposSpriteY = 62;
-let KLrecorte =82;
-let KArecorte =130;
-let KposX = 0;
-let KposY = 150;
+let KLrecorte = 82;
+let KArecorte = 130;
+let KposX = 200;
+let KposY = 0;
 let Klargura = 150;
 let Kaltura = 210;
 let Ksprites = 0;
 let Kmovimento = "parado";
 
+ // Definindo as dimensões da barra de vida
+ var barWidth = 300;
+ var barHeight = 30;
+
 luffybase.onload = () => { //espera a imagem carregar para depois abri-la
     //imagem, XiniRecorte, YiniRecorte, Lrecorte, Arecorte, Arecorte, posX, posy,Largura, Altura,
-    ctx.drawImage(luffybase, posSpriteX, 0, 45, 80, posX, posY, 85, 150)
+    ctx.drawImage(luffybase, posSpriteX, 0, 45, 80, posX, posY, 85, 150);
 }
-    kaidoBase.onload = () =>{
+kaidoBase.onload = () => {
     ctx.drawImage(kaidoBase, KposSpriteX, KposSpriteY, KLrecorte, KArecorte, KposX, KposY, Klargura, Kaltura);
+}
+
+ // Definindo a porcentagem inicial da barra de vida
+ var LhealthPercent = 100;
+ var KhealthPercent = 100;
+
+  // Desenhar a barra de vida inicial
+  drawHealthBar(ctx, 50, 500, barWidth, barHeight, LhealthPercent,"#008E31");
+  drawHealthBar(ctx, 500, 500, barWidth, barHeight, KhealthPercent,"#8C030E");
+
+// Função de Vida
+function drawHealthBar(ctx, x, y, width, height, percent,color) {
+    // Desenhar o fundo da barra
+    ctx.fillStyle = '#ccc';
+    ctx.fillRect(x, y, width, height);
+
+    // Desenhar a barra de vida atual
+    ctx.fillStyle = color;
+    ctx.fillRect(x, y, width * (percent/100), height);
 }
 
 let animaAvanco = 0; //Define se o sprite se a animação esta indo ou voltando
 
-function animacaoLuffy(){
+function animacaoLuffy() {
     switch (movimento) {
         case 'parado':
             posSpriteX = 0;
             posSpriteY = 0;
-            animaAvanco=0;
+            animaAvanco = 0;
             var parado = setInterval(() => {
                 ctx.clearRect(posX, posY, 85, 150);
-                
+
                 if (animaAvanco == 0) {
                     posSpriteX += 43;
                     if (posSpriteX >= 129) {
@@ -61,15 +92,16 @@ function animacaoLuffy(){
                         animaAvanco = 0;
                     }
                 }
+                
                 ctx.drawImage(luffybase, posSpriteX, posSpriteY, 45, 80, posX, posY, 85, 150);
 
                 if (movimento != 'parado') {
                     clearInterval(parado);
                 }
-            }, 400)
+            }, 200)
             break
 
-        case 'atacar':
+        case 'Pistol':
             posSpriteY = 270;
             sprites = 0;
             largura = 85;
@@ -129,6 +161,10 @@ function animacaoLuffy(){
                         posSpriteX = 520;
                         Lrecorte = 45;
                         largura = 80;
+
+                        //Diminui a vida
+                        KhealthPercent -= 5;
+                        drawHealthBar(ctx, 500, 500, barWidth, barHeight, KhealthPercent,"#8C030E");
                         break;
                     case 11:
                         movimento = "parado";
@@ -138,55 +174,73 @@ function animacaoLuffy(){
                 }
 
                 sprites++;
+                
                 ctx.drawImage(luffybase, posSpriteX, posSpriteY, Lrecorte, Arecorte, posX, posY, largura, 150);
-            }, 250)
+            }, 100)
     }
 }
 animacaoLuffy();
 
 let KanimaAvanco = 0;
 
-function animacaoKaido(){
-        Ksprites = 0;
+function animacaoKaido() {
 
-    var kparado = setInterval(() => {
-        ctx.clearRect(KposX, KposY, Klargura, Kaltura);
+    switch (Kmovimento) {
+        case 'parado':
+            Ksprites = 0;
+            var kparado = setInterval(() => {
+                ctx.clearRect(KposX, KposY, Klargura, Kaltura);
+                if (KanimaAvanco == 0) {
+                    Ksprites++;
+                    if (Ksprites == 3) {
+                        KanimaAvanco = 1;
+                    }
+                } else {
+                    Ksprites = 0;
+                    if (Ksprites == 0) {
+                        KanimaAvanco = 0;
+                    }
+                }
 
-        if (KanimaAvanco == 0) {
-            Ksprites++;
-            if (Ksprites==4) {
-                KanimaAvanco = 1;
-            }
-        }else{
-            Ksprites--;
-            if (Ksprites==0) {
-                KanimaAvanco = 0;
-            }
-        }
-        console.log(Ksprites)
+                console.log(Ksprites)
+                switch (Ksprites) {
+                    case 0:
+                        KposSpriteX = 32;
+                        break;
+                    case 1:
+                        KposSpriteX = 150;
+                        break;
+                    case 2:
+                        KposSpriteX = 263;
+                        break;
+                    case 3:
+                        KposSpriteX = 370;
+                        break;
+                }
 
-        switch(Ksprites){
-            case 0:
-                KposSpriteX = 32;
-                break;
-            case 1:
-                KposSpriteX = 150;
-                break;
-            case 2:
-                KposSpriteX = 263;
-                break;
-            case 3:
-                KposSpriteX = 370;
-                break;
-        }
-
-        //imagem, XiniRecorte, YiniRecorte, Lrecorte, Arecorte, Arecorte, posX, posy,Largura, Altura,
-        ctx.drawImage(kaidoBase, KposSpriteX, KposSpriteY, KLrecorte, KArecorte, KposX, KposY, Klargura, Kaltura);
-    }, 400)
+                // imagem, XiniRecorte, YiniRecorte, Lrecorte, Arecorte, Arecorte, posX, posy,Largura, Altura,
+                ctx.drawImage(kaidoBase, KposSpriteX, KposSpriteY, KLrecorte, KArecorte, KposX, KposY, Klargura, Kaltura);
+            }, 400)
+            break;
+        case 'attack':
+            var kattack = setInterval(() => {
+                ctx.clearRect(KposX, KposY, Klargura, Kaltura);
+                    
+                ctx.drawImage(kaidoBase, KposSpriteX, KposSpriteY, KLrecorte, KArecorte, KposX, KposY, Klargura, Kaltura);
+            }, 500)
+            break;
+        case 'dano':
+            var kdano = setInterval(() => {
+                ctx.clearRect(KposX, KposY, Klargura, Kaltura);
+                    
+                ctx.drawImage(kaidoBase, KposSpriteX, KposSpriteY, KLrecorte, KArecorte, KposX, KposY, Klargura, Kaltura);
+            },500) 
+    }
 }
 animacaoKaido();
 
-function atacar() {
-    movimento = "atacar";
+function Pistol() {
+    movimento = "Pistol";
     animacaoLuffy();
 }
+
