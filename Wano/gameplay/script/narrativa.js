d0edd8c18440c466507b58bbcd5e538e4e1712eb
Thiago <teyextreme@outlook.com>
2023-05-23 13:22:16 -0300

    teste

--- Wano/gameplay/script/narrativa.js
+++ Wano/gameplay/script/narrativa.js
@@ -10,24 +10,24 @@ const messages = [
   { text: "Luffy: Achei ele.", image: "narrativaImgs/7.png", audio: "" },
   { text: "Luffy: Kaido!", image: "narrativaImgs/8.png", audio: "" },
   { text: "Luffy: Desça la Momo.", image: "narrativaImgs/25.png", audio: "" },
-  { text: " ", image: "narrativaImgs/26.png", audio: "" },
+  { text: "**", image: "narrativaImgs/26.png", audio: "" },
   { text: "Luffy: Gear 4!", image: "narrativaImgs/28.png", audio: "" },
-  { text: "", image: "narrativaImgs/27.png", audio: "" },
-  { text: "", image: "narrativaImgs/29.png", audio: "" },
+  { text: "**", image: "narrativaImgs/27.png", audio: "" },
+  { text: "**", image: "narrativaImgs/29.png", audio: "" },
   { text: "Luffy: SNAKE MAN!!!", image: "narrativaImgs/30.png", audio: "" },
   { text: "Yamato: SHINSOKU!", image: "narrativaImgs/31.png", audio: "" },
   { text: "Yamato: HAKUJAKU!!", image: "narrativaImgs/32.png", audio: "" },
-  { text: " ", image: "narrativaImgs/33.png", audio: "" },
+  { text: "**", image: "narrativaImgs/33.png", audio: "" },
   { text: "Luffy: GOMU GOMU NO!!", image: "narrativaImgs/3.png", audio: "" },
   { text: "Luffy: JET CULVERIN!!", image: "narrativaImgs/6.png", audio: "" },
   { text: "Kaido: Oque?", image: "narrativaImgs/9.png", audio: "" },
-  { text: " ", image: "narrativaImgs/34.png", audio: "" },
-  { text: "", image: "narrativaImgs/12.png", audio: "" },
-  { text: "", image: "narrativaImgs/13.png", audio: "" },
-  { text: "", image: "narrativaImgs/14.png", audio: "" },
-  { text: "", image: "narrativaImgs/15.png", audio: "" },
-  { text: "", image: "narrativaImgs/16.png", audio: "" },
-  { text: "", image: "narrativaImgs/17.png", audio: "" },
+  { text: "**", image: "narrativaImgs/34.png", audio: "" },
+  { text: "**", image: "narrativaImgs/12.png", audio: "" },
+  { text: "**", image: "narrativaImgs/13.png", audio: "" },
+  { text: "**", image: "narrativaImgs/14.png", audio: "" },
+  { text: "**", image: "narrativaImgs/15.png", audio: "" },
+  { text: "**", image: "narrativaImgs/16.png", audio: "" },
+  { text: "**", image: "narrativaImgs/17.png", audio: "" },
   { text: "Luffy: Foi mal, Atrasei Porque presisava comer!", image: "narrativaImgs/10.png", audio: "" },
   { text: "Luffy: Deixa que eu cuido do resto!", image: "narrativaImgs/11.png", audio: "" },
 
@@ -51,6 +51,19 @@ function showMessage() {
   isTyping = true;
   animateText(message.text);
   imageBox.src = message.image;
+
+  // // Transição Imagens
+  // imageBox.classList.remove('show');
+
+  // // Aguarde um pequeno intervalo antes de atualizar o src para dar tempo para a transição ocorrer
+  // setTimeout(function() {
+  //   imageBox.src = message.image;
+
+  //   // Adicione a classe CSS 'show' para mostrar a nova imagem com transição
+  //   setTimeout(function() {
+  //     imageBox.classList.add('show');
+  //   }, 10);
+  // }, 300); 
 
   if (message.audio) {
     if (audioElement) {
@@ -115,5 +128,19 @@ function alterarVolume(volume) {
 document.addEventListener("click", reproduzirSom);
 
 
+function removerLoading() {
+  const loading = document.querySelector('.loading');
+  loading.style.opacity ="0";
+  setTimeout(function() {
+      document.body.removeChild(loading);
+    }, 300);
+}
+window.onload = function() {
+  showMessage();
+  setTimeout(function() {
+    // Simula o carregamento atrasado dos arquivos
+    removerLoading();
+  }, 500);
+};
 
 
